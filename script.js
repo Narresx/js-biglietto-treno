8a6a0d3852934125030b37315072c06c9fbb0ede
Narresx <92748159+Narresx@users.noreply.github.com>
2021-12-21 15:35:18 +0100

    add first point

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 - Va applicato uno sconto del 40% per gli over 65
 - L'output del prezzo finale va messo fuori in forma umana, ossia con massimo due decimali, per indicare centesimi sul prezzo.
 */
+
+/*  Procedimento
+
+1 - Chiediamo all'utente i Km che vuole percorrere in treno e la sua età
+2 - Calcolare il prezzo del biglietto in base alle varie variabili della traccia
+3 - Recupero un elemento in pagina e stampo il risultato
+*/
+
+// Punto 1
+
+const distanceKm = prompt('A quanto ammonta la distanza che vuoi percorrere? ( Indicare la distanza in chilometri)')
+console.log(distanceKm)
+const age = prompt('Quanti anni hai?')
+console.log(age)
+
+
