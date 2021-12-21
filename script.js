da9c5fafa648f713f2e68e81850d2342728336cb
Narresx <92748159+Narresx@users.noreply.github.com>
2021-12-21 15:20:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('JS OK')
+
+/* Traccia 
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere  e l'età del passeggero.
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto secondo queste regole:
+
+- Il prezzo del biglietto è definito in base ai km (0.21 al km)
+- Va applicato uno sconto del 20% per i minorenni
+- Va applicato uno sconto del 40% per gli over 65
+- L'output del prezzo finale va messo fuori in forma umana, ossia con massimo due decimali, per indicare centesimi sul prezzo.
+*/
